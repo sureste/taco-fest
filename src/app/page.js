99efb0bfa69bdcdf2ed0ce030967708e24cdd97b
Antonio Madrigal <edipean@hotmail.com>
2024-03-18 14:38:34 -0600

    final changes

--- src/app/page.js
+++ src/app/page.js
@@ -15,44 +15,51 @@ export default function Home() {
         </div>
 
         {/* Address && Date */}
-        <div className="pb-16">
-          <Image width={220} height={240} src="/assets/iztacalco.png" />
+        <div className="pb-16 flex justify-center">
+          <img className="w-2/3" src="/assets/iztacalco.png" />
         </div>
 
-        <div className="pb-16">
-          <Image width={340} height={240} src="/assets/FECHA.png" />
+        <div className="pb-16 flex justify-center">
+          <img className="w-2/3" src="/assets/FECHA.png" />
         </div>
 
         {/* Media */}
         <div className="pb-16">
           <video
+            className="sm:hidden"
+            src="/assets/videoPhone.mp4"
+            controls="controls"
+            autoPlay="true"
+          />
+          <video
+            className="max-md:hidden"
             src="/assets/videodesktop.mp4"
             controls="controls"
-            autoplay="true"
+            autoPlay="true"
           />
         </div>
 
-        <div>
-          <Image width={340} height={240} src="/assets/taco1.png" />
+        <div className="flex justify-center">
+          <img src="/assets/taco1.png" className=" w-4/6 max-md:w-2/3 " />
         </div>
       </div>
 
       {/* Requirements */}
       <div className="flex align-middle relative">
-        <img src="/assets/bg2.jpg" width='100%' />
-        <div className="absolute  self-center pl-16">
-          <img  src="/assets/requisitos.png" />
+        <img src="/assets/bg2.jpg" width="100%" />
+        <div className=" w-4/6 max-md:w-3/6 absolute  self-center pl-16 max-md:pl-5">
+          <img src="/assets/requisitos.png" />
         </div>
       </div>
 
       {/* Menu */}
       <div className="bg-background3 bg-no-repeat bg-cover flex flex-col items-center justify-center">
-        <div className="pt-24">
-          <Image width={600} height={300} src="/assets/con-queso.png" />
+        <div className="pt-24 flex justify-center ">
+          <img className="w-2/3" src="/assets/con-queso.png" />
         </div>
 
-        <div className="pt-16 pb-16">
-          <Image width={1200} height={600} src="/assets/lista-tacos.png" />
+        <div className="pt-16 pb-16 flex justify-center ">
+          <img className="w-3/3" src="/assets/lista-tacos.png" />
         </div>
       </div>
     </main>
